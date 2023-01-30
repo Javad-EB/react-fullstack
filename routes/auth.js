a4a1bb5826f221de58223c3ffce461669b3c4fc7
Javad <Javad.esmatib@Gmail.com>
2023-01-30 18:20:45 +0100

    Added authorisation to users

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const User = require("../models/User")
 const bcrypt = require('bcryptjs')
 const validateRegisterInput = require("../validation/registerValidation")
+const jwt = require('jsonwebtoken')
+const requireAuth = require('../middleware/permissions')
+
 
 // @route       GET /api/auth/test
 // @desc        Test the auth route
@@ -16,12 +19,12 @@ router.get("/test", (req, res) => {
 // @access      Public
 router.post("/register", async (req, res) => {
     try {
-        const {errors, isValid} = validateRegisterInput(req.body)
-        if(!isValid) {
+        const { errors, isValid } = validateRegisterInput(req.body)
+        if (!isValid) {
             return res.status(400).json(errors)
         }
         // Check for existing user
-        const existingEmail = await User.findOne({ 
+        const existingEmail = await User.findOne({
             email: new RegExp("^" + req.body.email + "$", "i")
         })
         if (existingEmail) {
@@ -41,8 +44,11 @@ router.post("/register", async (req, res) => {
         // save the user on the database
         const savedUser = await newUser.save()
 
+        const userToReturn = { ...savedUser._doc }
+        delete userToReturn.password
+
         // return the new user
-        return res.json(savedUser)
+        return res.json(userToReturn)
 
     } catch (err) {
         //error here
@@ -51,5 +57,69 @@ router.post("/register", async (req, res) => {
         res.status(500).send(err.message)
     }
 })
+
+// @route       POST /api/auth/login
+// @desc        Login user and return a access token
+// @access      Public
+router.post("/login", async (req, res) => {
+    try {
+        // check for the user
+        const user = await User.findOne({
+            email: new RegExp("^" + req.body.email + "$", "i")
+        })
+        if (!user) {
+            return res
+                .status(400)
+                .json({ error: "There was a problem with your login credentials" })
+        }
+
+        const passwordMatch = await bcrypt.compare(
+            req.body.password,
+            user.password
+        )
+
+        if (!passwordMatch) {
+            return res
+                .status(400)
+                .json({ error: "There was a problem with your login credentials" })
+        }
+
+        const payload = { userId: user._id }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        })
+
+        res.cookie("access-token", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        const userToReturn = { ...user._doc }
+        delete userToReturn.password
+
+        return res.json({
+            token: token,
+            user: userToReturn,
+        })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send(err.message)
+    }
+})
+
+// @route       GET /api/auth/current
+// @desc        Return the currently authed user
+// @access      private
+router.get("/current", requireAuth, (req, res) => {
+    if(!req.user){
+        return res.status(401).send("Unauthorized");
+    }
+
+    return res.json(req.user)
+})
+
 
 module.exports = router
