0b442c5627002fff572a01817264dc7b3fe7035b
Javad <Javad.esmatib@Gmail.com>
2023-02-03 20:24:48 +0100

    Rendering the ToDo List

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useGlobalContext } from '../context/GlobalContext'
 import { useNavigate } from 'react-router-dom'
+import ToDoCard from './ToDoCard'
 
 const Dashboard = () => {
-    const { user } = useGlobalContext()
+    const { user, completeToDos, incompleteToDos } = useGlobalContext()
     const navigate = useNavigate()
     React.useEffect(() => {
         if (!user && navigate) {
@@ -11,7 +12,30 @@ const Dashboard = () => {
         }
     }, [user, navigate])
 
-    return <h1>Dashboard</h1>
+    return (
+        <div className="dashboard">
+            <div className="todos">
+                {incompleteToDos.map(
+                    (toDo) => (
+                        <ToDoCard toDo={toDo} key={toDo._id} />
+                    )
+                )}
+            </div>
+            {completeToDos.length > 0 && (
+                <div className="todos">
+                    <h2 className="todos__title">
+                        Complete ToDo's
+                    </h2>
+                    {completeToDos.map(
+                        (toDo) => (
+                            <ToDoCard toDo={toDo} key={toDo._id} />
+                        )
+                    )}
+                </div>
+            )}
+
+        </div>
+    )
 }
 
 export default Dashboard
