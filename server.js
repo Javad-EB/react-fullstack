1380af904798abeed651a4723e97c109e8a1c946
Javad <Javad.esmatib@Gmail.com>
2023-02-07 09:56:52 +0100

    fix mistake

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
-const path = reqire('path')
+const path = require("path")
 
 
 //import routes
